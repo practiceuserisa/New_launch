f347a3ac8e7d8c24d238a019c0204d7a5b815b7f
AASTHAJUNEJA <53206006+AASTHAJUNEJA@users.noreply.github.com>
2019-07-23 10:10:39 +0530

    FACTORIAL.CPP
    This is the iterative method to find a factorial of a number

---
+++ fact.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,i , factorial = 1;
+cout<<"enter any number"<<endl;
+cin>>a;
+for(i = 1;i<a+1;i++)
+{
+factorial = factorial*i;
+}
+cout<<"the factorial of a number is "<< factorial<<endl;
+}
